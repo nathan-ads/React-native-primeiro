ecde323eb62088c5c5a25df91fc51024576e04d3
nathan <nathan.silva174@etec.sp.gov.br>
2025-04-03 20:17:00 -0300

    terceiro commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native-reanimated/lib/typescript/Animated";
+
+export default function HomeScreen() {
+
+    return(
+    <View>
+
+        <text>teste</text>
+        <Text>Um exemplo de texto</Text>
+    </View>
+    );
+
+}
