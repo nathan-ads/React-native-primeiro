0bde13ea8fc3f636286119c9f5f19bc88c673c8e
nathan <nathan.silva174@etec.sp.gov.br>
2025-04-03 22:00:44 -0300

    Quarto commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import { Text, View } from "react-native-reanimated/lib/typescript/Animated";
+import React, { useState } from "react";
+import { Button, Text, View } from "react-native";
 
 export default function HomeScreen() {
+    const [count, setCount] = useState(0);
 
     return(
     <View>
+        <Text>Bem-vindo ao meu App</Text>
+        <Text>Contador: {count}</Text>
 
-        <text>teste</text>
-        <Text>Um exemplo de texto</Text>
-    </View>
+        <Button title="Aumentar" onPress={() => {setCount( count + 1)}}/>
+
+        <Button title="Diminuir" onPress={() => {setCount( count - 1)}}/>
+
+        <Button title="Resetar" onPress={() => {setCount(0)}}/>
+        </View>
     );
 
 }
