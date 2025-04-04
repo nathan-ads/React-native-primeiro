6d4b0f2d09ee408b3ecc51094a46c4418e9dc63f
nathan <nathan.silva174@etec.sp.gov.br>
2025-04-03 22:01:25 -0300

    Quarto commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import CustomButton from "@/components/ui/Title";
 import React, { useState } from "react";
 import { Button, Text, View } from "react-native";
 
@@ -10,10 +11,12 @@ export default function HomeScreen() {
         <Text>Contador: {count}</Text>
 
         <Button title="Aumentar" onPress={() => {setCount( count + 1)}}/>
-
+        <br></br>
         <Button title="Diminuir" onPress={() => {setCount( count - 1)}}/>
-
+        <br></br>
         <Button title="Resetar" onPress={() => {setCount(0)}}/>
+        <br></br>
+            <CustomButton title="NOME"/>
         </View>
     );
 
